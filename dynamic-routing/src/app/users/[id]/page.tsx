679638ac724fffd183d17e2a78025ee7f7a1193b
Lalit-mahata <lm3654725@gmail.com>
2025-04-11 21:27:23 +0545

    updated

--- dynamic-routing/src/app/users/[id]/page.tsx
+++ dynamic-routing/src/app/users/[id]/page.tsx
@@ -5,18 +5,11 @@ interface userProps {
     };
   }
 const page = ({params}: userProps) => {
-    const person = [
-        {id: "1", name: "user1", age: 20},
-        {id: "2", name: "user2", age: 30}
+    const {id} = params
 
-    ]
-
-    const user = person.find((user)=> user.id === params.id)
   return (
     <div>
-       <h1>{user?.name}</h1>
-       <h1>{user?.id}</h1>
-       <h1>{user?.age}</h1>
+<h1>userProfile: {id}</h1>
 
     </div>
   )
